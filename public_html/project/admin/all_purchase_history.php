3c4cdbe80e0dd99775027eaf7bf67845574ecd13
AkramAbounaja <aa232@njit.edu>
2022-12-21 23:09:40 -0500

    Owner can see all Purchase History

--- public_html/project/admin/all_purchase_history.php
+++ public_html/project/admin/all_purchase_history.php
@@ -1,18 +1,19 @@
 <?php
 //note we need to go up 1 more directory
-require(__DIR__ . "/../../partials/nav.php");
-if (!is_logged_in()) {
-    flash("Need to be logged in to view orders", "warning");
-    header("location: login.php");
+require(__DIR__ . "/../../../partials/nav.php");
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: $BASE_PATH/home.php"));
 }
 $results = [];
 $db = getDB();
-$stmt = $db->prepare("SELECT Orders.id, Orders.total_price, Orders.payment_method, Orders.address FROM Orders
-where Orders.user_id = :user_id 
+$stmt = $db->prepare("SELECT Orders.id, Orders.total_price, Orders.payment_method, Orders.address, Users.username
+FROM Orders
+JOIN Users ON Orders.user_id = Users.id
 ORDER BY Orders.id DESC
-LIMIT 10"); 
+LIMIT 10");
 try {
-    $stmt->execute([":user_id" => get_user_id()]); //specify the user_id so I get only products in tat user's cart
+    $stmt->execute(); //specify the user_id so I get only products in tat user's cart
     $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $results = $r;
 } catch (PDOException $e) {
@@ -28,8 +29,8 @@ try {
         text-align: center;
     }
 </style>
-<h1 id="myCart" class="center">Your Orders</h1>
-    <p class ="center">Thank you for shopping!</p>
+<h1 id="myCart" class="center">All Orders</h1>
+    <!-- <p class ="center">Thank you for shopping!</p> -->
     <div class="row row-cols-1 row-cols-md-5 g-4">
         <?php if (count($results) > 0) : ?>
 
@@ -37,15 +38,16 @@ try {
                 <div id='orderwithID<?php echo $item["id"]; ?>' class="col">
                     <div class="card bg-light">
                         <div class="card-header">
-                        <p class="card-text">OrderID: <?php se($item, "id"); ?></p>
+                        <p class="card-text">Name: <?php se($item, "username"); ?></p>
                         </div>
                         <div class="card-body">
+                            <p class="card-text">OrderID: <?php se($item, "id"); ?></p>
                             <p class="card-text">Total price: $<?php se($item, "total_price"); ?></p>
                             <p class="card-text">Payment method: <?php se($item, "payment_method"); ?></p>
                             <p class="card-text">Deliever To: <?php se($item, "address"); ?></p>
                         </div>
                         <div class="card-footer">
-                        <a class="btn btn-primary" href="order_confirmation.php?prevOrderID=<?php echo se($item, "id"); ?>">More Details</a>
+                        <a class="btn btn-primary" href="admin_order_details.php?prevOrderID=<?php echo se($item, "id"); ?>">More Details</a>
                         </div>
                     </div>
                 </div>
@@ -54,5 +56,5 @@ try {
     </div>
 </div>
 <?php
-    require(__DIR__ . "/../../partials/footer.php");
+    require_once(__DIR__ . "/../../../partials/footer.php");
 ?>
